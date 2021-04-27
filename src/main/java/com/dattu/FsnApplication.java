ff35e8713e793da2c5df326d1d926583bdee1893
dattu <dattu@dattu-Lenovo-ideapad-330-15ARR>
2021-04-27 16:50:20 +0530

    1)Create Project FSN using Spring Starter 2)Adding required dependency Spring JPA,  MYSQL Driver, Spring Web, DevTool,Java Email 3)Create DataBase FSN

---
+++ src/main/java/com/dattu/FsnApplication.java
@@ -0,0 +1,13 @@
+package com.dattu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsnApplication.class, args);
+	}
+
+}
